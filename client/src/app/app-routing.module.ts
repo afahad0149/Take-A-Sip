ae0f139f7210f5a96f99bdd8810ba3054f5f2316
afahad0149 <fahad.akibahmed@gmail.com>
2023-03-12 09:58:02 +0600

    separated components and pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,29 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { FoodPageComponent } from './components/food-page/food-page.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FoodPageComponent } from './pages/food-page/food-page.component';
 import { ListComponent } from './components/list/list.component';
-import { KitchenComponent } from './components/kitchen/kitchen.component';
-import { OrderFormComponent } from './components/order-form/order-form.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { KitchenComponent } from './pages/kitchen/kitchen.component';
+import { OrderFormComponent } from './pages/order-form/order-form.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 import { AuthGuard } from './services/authGuard/auth.guard';
 import { AdminGuard } from './services/adminGuard/admin.guard';
 
 const routes: Routes = [
-  {path: "register", component: RegisterComponent},
-  {path: "login", component: LoginComponent},
-  {path: "kitchen", component: KitchenComponent, canActivate: [AuthGuard, AdminGuard]},
-  {path: "food/:id", component: FoodPageComponent, canActivate: [AuthGuard]},
-  {path: "order", component: OrderFormComponent, canActivate: [AuthGuard]},
-  {path: "list", component: ListComponent, canActivate: [AuthGuard]},
-  {path: "home", component: HomeComponent, canActivate: [AuthGuard]},
-  {path: '', redirectTo: "register", pathMatch: "full"},
-  {path: "**", component: HomeComponent, canActivate: [AuthGuard]}
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'kitchen',
+    component: KitchenComponent,
+    canActivate: [AuthGuard, AdminGuard],
+  },
+  { path: 'food/:id', component: FoodPageComponent, canActivate: [AuthGuard] },
+  { path: 'order', component: OrderFormComponent, canActivate: [AuthGuard] },
+  { path: 'list', component: ListComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: '**', component: HomeComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
