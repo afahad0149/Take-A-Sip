b52c5ef372f33442bfb02db874bf31eeb285f1e7
afahad0149 <fahad.akibahmed@gmail.com>
2023-03-12 13:01:20 +0600

    added form validation for email and password

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,68 +1,63 @@
 import { Component, EventEmitter, Output, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/authService/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
+  errorMsg: string = '';
+  loginForm = this.fb.group({
+    email: '',
+    password: new FormControl('', [Validators.minLength(6)]),
+  });
 
-errorMsg : string = '';
-loginForm = this.fb.group({
-  email:'',
-  password:''
-});
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
 
-constructor(private fb: FormBuilder, private auth : AuthService, private router: Router){}
-
-ngOnInit(): void {
-
-  //If logged in, not letting go to login page --> redirecting to own home
-  const userStr = localStorage.getItem('user');
-  if (userStr) {
-    const user = JSON.parse(userStr);
-    if (user.usertype === 'admin') {
-      this.router.navigate(['kitchen'])
-    } else {
-      this.router.navigate(['home'])
+  ngOnInit(): void {
+    //If logged in, not letting go to login page --> redirecting to own home
+    const userStr = localStorage.getItem('user');
+    if (userStr) {
+      const user = JSON.parse(userStr);
+      if (user.usertype === 'admin') {
+        this.router.navigate(['kitchen']);
+      } else {
+        this.router.navigate(['home']);
+      }
     }
   }
-}
 
+  login() {
+    const val = this.loginForm.value;
 
-login() {
-  const val = this.loginForm.value;
+    if (val.email && val.password) {
+      // if (val.password.length < 2) {
+      //   this.errorMsg = 'Your password must be atleast 6 characters long.';
+      // } else {
+      this.auth.login(val.email, val.password).subscribe({
+        next: (res: any) => {
+          localStorage.setItem('accessToken', res.headers.get('authorization'));
+          localStorage.setItem('user', JSON.stringify(res.body.user));
 
-  if (val.email && val.password) {
-      if (val.password.length < 2) {
-        this.errorMsg = 'Your password must be atleast 6 characters long.'
-      } else {
-        this.auth.login(val.email, val.password).subscribe({
-          next: (res: any) => {
-            localStorage.setItem('accessToken', res.headers.get('authorization'));
-            localStorage.setItem('user', JSON.stringify(res.body.user));
-            
-            /*For going to page after loggin in without reload */
-            if (res.body.user.usertype === 'admin') {
-              this.router.navigate(['kitchen']);
-    
-            } else {
-              this.router.navigate(['home']);
-            }
+          /*For going to page after loggin in without reload */
+          if (res.body.user.usertype === 'admin') {
+            this.router.navigate(['kitchen']);
+          } else {
+            this.router.navigate(['home']);
+          }
         },
-        error: error => this.errorMsg = error.error
-        });
-      }
-      
-  } else {
-    this.errorMsg = 'Please enter email and password.'
+        error: (error) => (this.errorMsg = error.error),
+      });
+      // }
+    } else {
+      this.errorMsg = 'Please enter email and password.';
+    }
   }
-}
-
-
-
-
 }
--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { faMugHot } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/authService/auth.service';
@@ -7,64 +7,84 @@ import { AuthService } from 'src/app/services/authService/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent implements OnInit {
   mugIcon = faMugHot;
-  errorMsg : string = '';
-  
+  errorMsg: string = '';
+
   regForm = this.fb.group({
     firstName: '',
     lastName: '',
     designation: '',
     email: '',
-    password: '',
-    confirmPassword:''
+    password: new FormControl('', [Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.minLength(6)]),
   });
-   
-  constructor(private fb: FormBuilder, private auth : AuthService, private router: Router){}
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-  //If logged in, not letting go to register page --> redirecting to own home
+    //If logged in, not letting go to register page --> redirecting to own home
     const userStr = localStorage.getItem('user');
     if (userStr) {
       const user = JSON.parse(userStr);
       if (user.usertype === 'admin') {
-        this.router.navigate(['kitchen'])
+        this.router.navigate(['kitchen']);
       } else {
-        this.router.navigate(['home'])
+        this.router.navigate(['home']);
       }
     }
   }
 
   register() {
     const val = this.regForm.value;
-  
-    if (val.firstName && val.lastName && val.designation && val.email && val.password) {
-      if (val.password.length < 6) {
-        this.errorMsg = 'Your password must be atleast 6 characters long.'
-      } else if (val.password != val.confirmPassword) {
-        this.errorMsg = "Your password doesn't match."
-      } else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val.email))) {
-        this.errorMsg = 'You have entered an invalid email address!'
+
+    if (
+      val.firstName &&
+      val.lastName &&
+      val.designation &&
+      val.email &&
+      val.password
+    ) {
+      // if (val.password.length < 6) {
+      //   this.errorMsg = 'Your password must be atleast 6 characters long.';
+      // } else
+      if (val.password != val.confirmPassword) {
+        this.errorMsg = "Your password doesn't match.";
+      } else if (
+        !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val.email)
+      ) {
+        this.errorMsg = 'You have entered an invalid email address!';
+      } else {
+        this.auth
+          .register(
+            val.firstName,
+            val.lastName,
+            val.designation,
+            val.email,
+            val.password
+          )
+          .subscribe({
+            next: (res: any) => {
+              localStorage.setItem(
+                'accessToken',
+                res.headers.get('authorization')
+              );
+              localStorage.setItem('user', JSON.stringify(res.body.newUser));
+
+              /*For going to page after loggin in without reload */
+              this.router.navigate(['home']);
+            },
+            error: (error) => (this.errorMsg = error.error),
+          });
       }
-      else {
-        this.auth.register(val.firstName,val.lastName,val.designation,val.email,val.password).subscribe({
-          next: (res: any) => {
-          localStorage.setItem('accessToken', res.headers.get('authorization'));
-          localStorage.setItem('user', JSON.stringify(res.body.newUser));
-
-          /*For going to page after loggin in without reload */
-          this.router.navigate(['home']);
-        },
-        error: error => this.errorMsg = error.error
-      });
-    }
     } else {
-      this.errorMsg = 'Please enter your information.'
-    }  
+      this.errorMsg = 'Please enter your information.';
+    }
   }
-
-
-
 }
